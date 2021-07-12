d34693631fce788a80fd614aaf3ae843201c167d
arturo <arturplur@gmail.com>
2021-07-12 18:45:59 -0500

    json

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 import requests
 import os
+import json
 
 TW_KEY = os.environ.get('TW_API_KEY')
 TW_URL = os.environ.get('TW_API_URL')
@@ -11,7 +12,10 @@ app = Flask(__name__)
 def projects():
     url = TW_URL + "/projects.json"
     resp = requests.get(url, auth=(TW_KEY,"d"))
-    return(resp.content)
+    projects = resp.content
+    with open('projects.txt', 'w') as outfile:
+        json.dump(projects, outfile)
+    return("dumped")
 
 @app.route("/tasks")
 def tasks():
