468d553f9a77aabb59263d2237867b488fb592e5
arturo <arturplur@gmail.com>
2021-07-13 00:13:40 -0500

    file uploads to cloud storage

--- app.py
+++ app.py
@@ -2,9 +2,18 @@ from flask import Flask
 import requests
 import os
 import json
+from google.cloud import storage
+
 
 TW_KEY = os.environ.get('TW_API_KEY')
 TW_URL = os.environ.get('TW_API_URL')
+
+'''
+Cloud Storage Env vars.
+'''
+GOOGLE_APPLICATION_CREDENTIALS = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+STORAGE_BUCKET_NAME = os.environ.get('STORAGE_BUCKET_NAME')
+bucket_name = STORAGE_BUCKET_NAME
 
 app = Flask(__name__)
 
@@ -12,6 +21,8 @@ app = Flask(__name__)
 def starred():
     '''
     https://developer.teamwork.com/projects/api-v3/ref/projects/get-projects-api-v3-projects-starredjson
+    starred() gets starred projects for a single user.
+    response from TW request is decoded from bytes and written as a json file at root.
     '''
     url = TW_URL + "/projects/api/v3/projects/starred.json"
     resp = requests.get(url, auth=(TW_KEY,"d"))
@@ -19,5 +30,37 @@ def starred():
     starred = starred.decode()
     with open('starred.json', 'w') as outfile:
         json.dump(starred, outfile)
+    
+
+    '''
+    initiallize cloud storage
+    '''
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    '''
+    Upload files to bucket in Storage.
+    '''
+    def upload(blob_name,file_path,bucket_name):
+        try:
+            bucket = storage_client.get_bucket(bucket_name)
+            blob = bucket.blob(blob_name)
+            blob.upload_from_filename(file_path)
+            return True
+        
+        except Exception as e:
+            print(e)
+            return False
+
+    file_path = 'starred.json'
+    upload(
+        blob_name = 'test',
+        file_path = file_path,
+        bucket_name = bucket
+
+    )
+
+
+
+
     return('200')
     
